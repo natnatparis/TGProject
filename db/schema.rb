079e6f6a6cd6c7321697e720a1a681691274e555
franck237 <franck.kamanou@gmail.com>
2019-08-01 22:26:27 +0200

    Ajout des tables Gossip et Tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_173812) do
+ActiveRecord::Schema.define(version: 2019_08_01_191418) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,21 @@ ActiveRecord::Schema.define(version: 2019_08_01_173812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gossips_on_user_id"
+  end
+
+  create_table "join_gotags", force: :cascade do |t|
+    t.integer "gossip_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gossip_id"], name: "index_join_gotags_on_gossip_id"
+    t.index ["tag_id"], name: "index_join_gotags_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@ require 'faker'
 
 City.destroy_all
 User.destroy_all
+Gossip.destroy_all
+Tag.destroy_all
+JoinGotag.destroy_all
 
 	random_city_list = []
 10.times do
@@ -22,11 +25,28 @@ end
 	random_user_list << random_user
 end
 
+	random_gossip_list = []
 20.times do
 	random_gossip = Gossip.create(title: Faker::Book.title, content: Faker::Lorem.sentence(word_count: 9, supplemental: false, random_words_to_add: 10), user: random_user_list.sample)
+	random_gossip_list << random_gossip
 end
 
+	random_tag_list = []
+10.times do
+	random_tag = Tag.create(title: Faker::Lorem.words(number: 1))
+	random_tag_list << random_tag
+end
 
+	i=0
+20.times do
+	random_join_gotag = JoinGotag.create(gossip: random_gossip_list[i], tag: random_tag_list.sample)
+	i = i + 1
+end
+
+40.times do
+	i = rand(1..20)
+	random_join_gotag = JoinGotag.create(gossip: random_gossip_list[i], tag: random_tag_list.sample)
+end
 #10.times do
 #	random_user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 9, supplemental: false, random_words_to_add: 10), email: Faker::Internet.email, age: Faker::Number.within(range: 18..70), city_id: random_user_list.sample)
 #end
